1278f74a4274f2723c2d58159885d15102508473
Roman Grishin <roman.grishin.me@gmail.com>
2022-07-22 16:12:28 +0300

    menu js (not complited)

---
+++ script/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const burger = document.querySelector('.menu__list__button'),
+      menuList = document.querySelector('.nav__list');
+
+burger.addEventListener('click', () => {
+    menuList.classList.add('active');
+});
